11a57b2cadae36bdb2953283aef3a6f58f37ebbf
Eric Silberstein <ericsilberstein1@gmail.com>
2023-05-17 19:22:17 +0000

    Add file upload to script and folder for json file

--- main.mjs
+++ main.mjs
@@ -1,18 +1,19 @@
 import { LokaliseApi } from "@lokalise/node-api";
+import fs from 'fs';
 
 async function main() {
   const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
   const project = await lokaliseApi.projects().create({ 
      //Add project name, description, languages, teamID. See documentation here: https://developers.lokalise.com/reference/create-a-project 
-    name: "API creation test 1", 
-    description: "Braze Campaign 1 description",
+    name: "Luz's Project", //project name
+    description: "My baby loves me", //project description
     team_id: "327867",
     languages: [
-        {lang_iso: "en_US"},
-        {lang_iso: "es"}], 
-    base_lang_iso: "en_US",
+        {lang_iso: "en_US"}, //Add base language ISO to project languages
+        {lang_iso: "es"}],  //Add target languages
+    base_lang_iso: "en_US", //Define the base language of the project from the language iso list
     project_type: "localization_files", 
-    tag: "Braze",
+    tag: "Braze", // This object's purpose is to add a tag at the project level for filtering in the dashboard. Current API does not support this endpoint :(
     per_platform_key_names: false,
     reviewing: true,
     auto_toggle_unverified: true,
@@ -32,14 +33,16 @@ async function main() {
   console.log(process.status); // => 'queued'
 
   await addContributors(projID);//Pass project ID to the contributors function
+  // await createKeys(projID);//Pass project ID to the file upload function
+  await fileUpload(projID);
 }
 
 async function addContributors(projectID) {
     const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
     const contributors = await lokaliseApi.contributors().create([
         {
-          "email": "bubbles.lokalise@gmail.com",
-          "fullname": "Bubblesr",
+          "email": "luzsmeyin@gmail.com",
+          "fullname": "Luz S",
           "is_admin": false,
           "is_reviewer": true,
           "languages": [
@@ -58,5 +61,58 @@ async function addContributors(projectID) {
     console.log(contributors);
   };
 
+  // async function createKeys(projectID) {
+  //   const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
+  //   const keys = await lokaliseApi.keys().create(
+  //       {
+  //         keys: [
+  //           {
+  //             key_name: "welcome_web",
+  //             description: "Index app welcome",
+  //             platforms: ["web"],
+  //             filenames: {
+  //               web: "my_filename.json",
+  //             },
+  //             translations: [
+  //               {
+  //                 language_iso: "en_US",
+  //                 translation: "Welcome",
+  //               },
+  //             ],
+  //           }
+  //         ],
+  //       },
+  //       { project_id : projectID }
+  //     );
+      
+  //     keys.items[0].platforms;
+  //     //keys.errors[0].message; // If some keys were not created, the errors will be listed here
+  //     console.log(keys);
+  // }
+
+async function fileUpload(projectID) {
+  
+  const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
+
+  // Read file contents
+  const filePath = 'BrazeJsonFiles/campaign1.json'; //Write name of filepath here
+  const fileContent = fs.readFileSync(filePath);
+
+  // Convert file content to Base64
+  const fileBase64 = Buffer.from(fileContent).toString('base64');
+
+  // Upload the file to Lokalise
+  const projectId = projectID;
+  const uploadResponse = await lokaliseApi.files().upload(projectId, {
+    data: fileBase64,
+    filename: 'en_US.json', //This is the name of the file that will be added to the keys as metadata. It can be the same as the filename used for variable filePath
+    lang_iso: 'en_US'
+  });
+
+  console.log(uploadResponse);
+}
+
 main().catch(console.error);
 addContributors().catch(console.error);
+// createKeys().catch(console.error);
+fileUpload().catch(console.error);
