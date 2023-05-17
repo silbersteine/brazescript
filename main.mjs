54fe2e19525799d5bfb20bf1a893584f4ac43111
Eric Silberstein <ericsilberstein1@gmail.com>
2023-05-17 16:29:55 +0000

    Create project with languages and settings

--- main.mjs
+++ main.mjs
@@ -2,8 +2,30 @@ import { LokaliseApi } from "@lokalise/node-api";
 
 async function main() {
   const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
-  const projects = await lokaliseApi.projects().list();
-  console.log(projects);
+  const project = await lokaliseApi.projects().create({ 
+     //Add project name, description, languages, teamID. See documentation here: https://developers.lokalise.com/reference/create-a-project 
+    name: "Project name", 
+    description: "Braze Campaign 1 description",
+    languages: [
+        {lang_iso: "en_US"},
+        {lang_iso: "es"}], 
+    base_lang_iso: "en_US",
+    project_type: "localization_files", 
+    tag: "Braze",
+    per_platform_key_names: false,
+    reviewing: true,
+    auto_toggle_unverified: true,
+    offline_translation: false,
+    key_editing: true,
+    inline_machine_translations: true,
+    custom_translation_statuses: true,
+    custom_translation_statuses_allow_multiple: false,
+    segmentation: false
+
+});
+  project.project_id;
+  console.log(project.project_id);
+  console.log(project);
 
   console.log(process.status); // => 'queued'
 }
