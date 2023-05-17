b391be1e780d958fa4cf05d9f640f78385114082
Eric Silberstein <ericsilberstein1@gmail.com>
2023-05-17 19:40:23 +0000

    Added comments explaining code and cleaned code

--- main.mjs
+++ main.mjs
@@ -1,12 +1,19 @@
+// Importing necessary modules and libraries
 import { LokaliseApi } from "@lokalise/node-api";
 import fs from 'fs';
 
-async function main() {
+// createProject function that creates the project
+async function createProject() {
+
+  // Initializing the Lokalise API client with the API key
   const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
-  const project = await lokaliseApi.projects().create({ 
-     //Add project name, description, languages, teamID. See documentation here: https://developers.lokalise.com/reference/create-a-project 
-    name: "Luz's Project", //project name
-    description: "My baby loves me", //project description
+  
+  // Creating a Lokalise project  
+  const project = await lokaliseApi.projects().create({
+     
+    // Setting project details such as name, description, and languages See documentation here: https://developers.lokalise.com/reference/create-a-project 
+    name: "Project Name", //project name
+    description: "Description", //project description
     team_id: "327867",
     languages: [
         {lang_iso: "en_US"}, //Add base language ISO to project languages
@@ -27,22 +34,29 @@ async function main() {
 
   project.project_id;
   const projID = project.project_id
+
+  // Printing the project ID and details to console
   console.log("The project ID is: " + project.project_id);
   console.log(project);
 
   console.log(process.status); // => 'queued'
 
-  await addContributors(projID);//Pass project ID to the contributors function
-  // await createKeys(projID);//Pass project ID to the file upload function
+  //Pass project ID to the other functions
+  await addContributors(projID);
   await fileUpload(projID);
 }
 
+
+// Function to add contributors to the project
 async function addContributors(projectID) {
     const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
+    
+    // Creating a contributor with email, name, and language permissions
+    //To add more contributors unhide the second contributo that is commented out. Make sure to leave a comma after the final closing {} for each contributor 
     const contributors = await lokaliseApi.contributors().create([
         {
-          "email": "luzsmeyin@gmail.com",
-          "fullname": "Luz S",
+          "email": "example@email.com",
+          "fullname": "Example",
           "is_admin": false,
           "is_reviewer": true,
           "languages": [
@@ -55,41 +69,29 @@ async function addContributors(projectID) {
                 "is_writable": true
             }
           ]
-        }
+        }// ,
+      //   "email": "example@email.com",
+      //   "fullname": "Example",
+      //   "is_admin": false,
+      //   "is_reviewer": true,
+      //   "languages": [
+      //     {
+      //       "lang_iso": "en_US",
+      //       "is_writable": false
+      //     },
+      //     {
+      //         "lang_iso": "es",
+      //         "is_writable": true
+      //     }
+      //   ]
+      // }
     ], { project_id: projectID });
 
     console.log(contributors);
   };
 
-  // async function createKeys(projectID) {
-  //   const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
-  //   const keys = await lokaliseApi.keys().create(
-  //       {
-  //         keys: [
-  //           {
-  //             key_name: "welcome_web",
-  //             description: "Index app welcome",
-  //             platforms: ["web"],
-  //             filenames: {
-  //               web: "my_filename.json",
-  //             },
-  //             translations: [
-  //               {
-  //                 language_iso: "en_US",
-  //                 translation: "Welcome",
-  //               },
-  //             ],
-  //           }
-  //         ],
-  //       },
-  //       { project_id : projectID }
-  //     );
-      
-  //     keys.items[0].platforms;
-  //     //keys.errors[0].message; // If some keys were not created, the errors will be listed here
-  //     console.log(keys);
-  // }
 
+// Function to upload a file to the project
 async function fileUpload(projectID) {
   
   const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
@@ -105,14 +107,14 @@ async function fileUpload(projectID) {
   const projectId = projectID;
   const uploadResponse = await lokaliseApi.files().upload(projectId, {
     data: fileBase64,
-    filename: 'en_US.json', //This is the name of the file that will be added to the keys as metadata. It can be the same as the filename used for variable filePath
+    //This is the name of the file that will be added to the keys as metadata. It can be the same as the filename used for variable filePath
+    filename: 'en_US.json', 
     lang_iso: 'en_US'
   });
 
   console.log(uploadResponse);
 }
 
-main().catch(console.error);
+createProject().catch(console.error);
 addContributors().catch(console.error);
-// createKeys().catch(console.error);
 fileUpload().catch(console.error);
