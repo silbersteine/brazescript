7ced55ec0b8938c75edb274fbd29c76ad60d27b1
Eric Silberstein <ericsilberstein1@gmail.com>
2023-05-18 20:45:59 +0000

    Externalize var for definition at top of script

--- main.mjs
+++ main.mjs
@@ -2,6 +2,21 @@
 import { LokaliseApi } from "@lokalise/node-api";
 import fs from 'fs';
 
+
+//Lokalise Authenticaion
+const apiToken = "72439dd7b4351377a6a3d143ea1cafe3931e81af"
+
+//Project Configuration
+const projectName = "string"
+const projectDescription = "string"
+const teamID = "327867"
+const baseLang = "en_US"
+
+//File Upload Configuration
+const file = "BrazeJsonFiles/campaign1.json"
+
+
+
 // createProject function that creates the project
 async function createProject() {
 
@@ -12,15 +27,15 @@ async function createProject() {
   const project = await lokaliseApi.projects().create({
      
     // Setting project details such as name, description, and languages See documentation here: https://developers.lokalise.com/reference/create-a-project 
-    name: "Project Name", //project name
-    description: "Description", //project description
-    team_id: "327867",
+    name: projectName, //project name
+    description: projectDescription, //project description
+    team_id: teamID,
     languages: [
-        {lang_iso: "en_US"}, //Add base language ISO to project languages
-        {lang_iso: "es"}],  //Add target languages
-    base_lang_iso: "en_US", //Define the base language of the project from the language iso list
+        {lang_iso: baseLang}, //Add base language ISO to project languages
+        {lang_iso: "es"}],  //Add target languages after the } and before the ],
+    base_lang_iso: baseLang, //Define the base language of the project from the language iso list
     project_type: "localization_files", 
-    tag: "Braze", // This object's purpose is to add a tag at the project level for filtering in the dashboard. Current API does not support this endpoint :(
+    // tag: "Braze", // This object's purpose is to add a tag at the project level for filtering in the dashboard. Current API does not support this endpoint :(
     per_platform_key_names: false,
     reviewing: true,
     auto_toggle_unverified: true,
@@ -49,7 +64,7 @@ async function createProject() {
 
 // Function to add contributors to the project
 async function addContributors(projectID) {
-    const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
+    const lokaliseApi = new LokaliseApi({ apiKey: apiToken });
     
     // Creating a contributor with email, name, and language permissions
     //To add more contributors unhide the second contributo that is commented out. Make sure to leave a comma after the final closing {} for each contributor 
@@ -94,10 +109,10 @@ async function addContributors(projectID) {
 // Function to upload a file to the project
 async function fileUpload(projectID) {
   
-  const lokaliseApi = new LokaliseApi({ apiKey: '72439dd7b4351377a6a3d143ea1cafe3931e81af' });
+  const lokaliseApi = new LokaliseApi({ apiKey: apiToken });
 
   // Read file contents
-  const filePath = 'BrazeJsonFiles/campaign1.json'; //Write name of filepath here
+  const filePath = file; //Import filepath here
   const fileContent = fs.readFileSync(filePath);
 
   // Convert file content to Base64
@@ -108,8 +123,8 @@ async function fileUpload(projectID) {
   const uploadResponse = await lokaliseApi.files().upload(projectId, {
     data: fileBase64,
     //This is the name of the file that will be added to the keys as metadata. It can be the same as the filename used for variable filePath
-    filename: 'en_US.json', 
-    lang_iso: 'en_US'
+    filename: file, 
+    lang_iso: baseLang
   });
 
   console.log(uploadResponse);
@@ -118,3 +133,4 @@ async function fileUpload(projectID) {
 createProject().catch(console.error);
 addContributors().catch(console.error);
 fileUpload().catch(console.error);
+
